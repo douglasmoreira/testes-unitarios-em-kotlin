a301a082d2b78ce2fcd8cd0e3357ad36ec24939d
Douglas <douglas.cosmo7@gmail.com>
2020-12-02 11:53:15 -0300

    forma de dividir um teste

--- src/test/kotlin/com/douglas/TestesUnitariosKotlin/servicos/LocacaoServiceTest.kt
+++ src/test/kotlin/com/douglas/TestesUnitariosKotlin/servicos/LocacaoServiceTest.kt
@@ -2,12 +2,21 @@ package com.douglas.TestesUnitariosKotlin.servicos
 
 import com.douglas.TestesUnitariosKotlin.entidades.Filme
 import com.douglas.TestesUnitariosKotlin.entidades.Usuario
-import com.douglas.TestesUnitariosKotlin.utils.DataUtils
+import com.douglas.TestesUnitariosKotlin.utils.DataUtils.isMesmaData
+import com.douglas.TestesUnitariosKotlin.utils.DataUtils.obterDataComDiferencaDias
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.*
 import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ErrorCollector
 import java.util.*
 
 class LocacaoServiceTest {
+
+    @get:Rule
+//    @Rule @JvmField // outra forma de anotar e tornar a Rule publica
+    var error = ErrorCollector()
 
     @Test
     fun main() {
@@ -21,7 +30,21 @@ class LocacaoServiceTest {
 
         //verificaçao
         Assert.assertTrue(locacao.valor == 5.0)
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.dataLocacao, Date()))
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.dataRetorno, DataUtils.obterDataComDiferencaDias(1)))
+        Assert.assertTrue(isMesmaData(locacao.dataLocacao, Date()))
+        Assert.assertTrue(isMesmaData(locacao.dataRetorno, obterDataComDiferencaDias(1)))
+
+        //teste usando o ErrorCollector
+
+        //teste usando o ErrorCollector
+        error.checkThat(locacao.valor, `is`(equalTo(5.0)))
+        error.checkThat(isMesmaData(locacao.dataLocacao, Date()), `is`(true))
+        error.checkThat(isMesmaData(locacao.dataRetorno, obterDataComDiferencaDias(1)), `is`(true))
+
+
+        //Estudo assertThat()
+        Assert.assertThat(locacao.valor, `is`(equalTo(5.0)))
+        Assert.assertThat(locacao.valor, `is`(not(6.0)))
+        Assert.assertThat(isMesmaData(locacao.dataLocacao, Date()), CoreMatchers.`is`(true))
+        Assert.assertThat(isMesmaData(locacao.dataRetorno, obterDataComDiferencaDias(1)), `is`(true))
     }
 }
